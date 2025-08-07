81c8b0a5f76b7624c9da8b4fb4f7774857b1195e
Modin <eom3658@gmail.com>
2025-08-07 22:23:16 +0900

    feat: 플레이어 캐릭터 이동, 카메라 회전

--- Source/ProjectA/Private/Character/PACharacter.cpp
+++ Source/ProjectA/Private/Character/PACharacter.cpp
@@ -4,37 +4,108 @@
 #include "Character/PACharacter.h"
 
 #include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
 APACharacter::APACharacter()
 {
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
-	InitializeCamera();
-	InitializeCharacterMovement();
+	SetupRotation();
+	SetupCamera();
+	SetupCharacterMovement();
 }
 
 void APACharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	InitializeInputMappingContext();
 }
 
-void APACharacter::InitializeCamera()
+void APACharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	// 1. SpringArm 생성 및 초기화
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	if (UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInput->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APACharacter::OnMove);
+		EnhancedInput->BindAction(LookAction, ETriggerEvent::Triggered, this, &APACharacter::OnLook);
+	}
+}
+
+void APACharacter::SetupRotation()
+{
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
+
+void APACharacter::SetupCamera()
+{
+	/*
+	 * 카메라 초기화
+	 *  - Proc 1. SpringArm 생성 및 초기화
+	 *  - Proc 2. Camera 생성 및 초기화
+	 */
+
+	// Proc 1
 	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
 	SpringArm->SetupAttachment(RootComponent);
-	SpringArm->TargetArmLength = 300.f;
+	SpringArm->TargetArmLength = 400.f;
+	SpringArm->SetRelativeRotation(FRotator(-30.f, 0.f, 0.f));
 	SpringArm->bUsePawnControlRotation = true;
 	
-	// 2. Camera 생성 및 초기화
+	// Proc 2
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	FollowCamera->SetupAttachment(SpringArm);
 	FollowCamera->bUsePawnControlRotation = false;
 }
 
-void APACharacter::InitializeCharacterMovement()
+void APACharacter::SetupCharacterMovement()
 {
+	UCharacterMovementComponent* characterMovement = GetCharacterMovement();
+	characterMovement->bOrientRotationToMovement = true;
+	characterMovement->RotationRate = RotationRate;
+}
 
+void APACharacter::InitializeInputMappingContext()
+{
+	if (APlayerController* PC = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem =
+			ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(MappingContext, 0);
+		}
+	}
+}
+
+void APACharacter::OnMove(const FInputActionValue& Value)
+{
+	// 캐릭터가 바라보는 방향을 기준으로 움직인다.
+	
+	FVector2D MovementVector = Value.Get<FVector2D>();
+	if (Controller)
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+		const FVector Forward = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const FVector Right = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		AddMovementInput(Forward, MovementVector.Y);
+		AddMovementInput(Right, MovementVector.X);
+	}
+}
+
+void APACharacter::OnLook(const FInputActionValue& Value)
+{
+	FVector2D LookVector = Value.Get<FVector2D>();
+	if (Controller)
+	{
+		AddControllerYawInput(LookVector.X);
+		AddControllerPitchInput(LookVector.Y);
+	}
 }
--- Source/ProjectA/Public/Character/PACharacter.h
+++ Source/ProjectA/Public/Character/PACharacter.h
@@ -4,7 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "PACharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class PROJECTA_API APACharacter : public ACharacter
@@ -16,14 +22,33 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
 private:
-	void InitializeCamera();
-	void InitializeCharacterMovement();
+	void SetupCamera();
+	void SetupRotation();
+	void SetupCharacterMovement();
+	
+	void InitializeInputMappingContext();
+
+	void OnMove(const FInputActionValue& Value);
+	void OnLook(const FInputActionValue& Value);
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* SpringArm;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* SpringArm;
+	UCameraComponent* FollowCamera;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* FollowCamera;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character Movement", meta = (AllowPrivateAccess = "true"))
+	FRotator RotationRate = FRotator(0.f, 500.f, 0.f);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputMappingContext* MappingContext;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	UInputAction* LookAction;
 };
